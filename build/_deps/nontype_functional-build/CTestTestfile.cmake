da328d33967b24dc9ee62a577ddad8075f3573b6
skrepka315 <skrepka315@gmail.com>
2026-03-27 19:01:19 +0300

    Delete build directory

--- build/_deps/nontype_functional-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/GeodeMods/MusicDownloader/build/_deps/nontype_functional-src
-# Build directory: C:/GeodeMods/MusicDownloader/build/_deps/nontype_functional-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
